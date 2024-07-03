0e898a99f40c050a73e9c99258a82b7a9797f74e
Laissy <laissydominique@gmail.com>
2024-07-03 14:16:42 -0300

    atualização..

--- doc.js
+++ doc.js
@@ -1,11 +1,28 @@
-const blocoPrincipal = document.querySelector('.bloco_principal');
-const segundoBloco = document.querySelector('.ja_tem_conta');
-const login = document.querySelector('.login');
-const ja_tem_conta = document.querySelector('.ja_tem_conta');
+document.addEventListener("DOMContentLoaded", function (event) {
+  const blocoPrincipal = document.querySelector(".bloco_principal");
+  const segundoBloco = document.querySelector(".ja_tem_conta");
+  const login = document.querySelector(".login");
+  const ja_tem_conta = document.querySelector(".ja_tem_conta");
+  const google = document.querySelector(".google");
+  const face = document.querySelector(".face");
+  const apple = document.querySelector(".apple");
+  const telefone = document.querySelector("#usar_tel");
+  const end_de_email = document.querySelector(".end_de_email");
 
-ja_tem_conta.addEventListener('click', () =>{
-    blocoPrincipal.classList.add("hidden");
-    segundoBloco.classList.add("hidden");
+  const email = document.querySelector(".input_email");
 
-    login.classList.remove("hidden");
-})
+  telefone.addEventListener("click", () => {
+    if (
+      email.placeholder === "nome@dominio.com" &&
+      telefone.textContent === "Usar número de telefone" 
+    ) {
+      email.placeholder = "(XX) xxxxx-xxxx";
+      telefone.innerHTML = "Usar email";
+    } else {
+      email.placeholder = "nome@dominio.com";
+      telefone.innerHTML = "Usar número de telefone";
+    }
+
+    
+  });
+});
